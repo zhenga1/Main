0c016ec1d03f7559e1d660cd90ec2bcd0dddb970
Aaron Zheng <aaronhaowenzheng@gmail.com>
2022-12-01 11:11:48 -0800

    organization

--- docs/autocommit.bat
+++
@@ -1,3 +0,0 @@
-git add -u                               REM :: Only add tracked files
-git commit -m "Aaron's life autocommit scheduler"      REM :: Commit all your added files
-git push origin master                   REM :: Branch (i.e. master)
